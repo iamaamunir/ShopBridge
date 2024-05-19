c8b73666f1fd185b588daeb7a2942ebc676a3b22
Munir Abdullahi <abdullahimunir3@gmail.com>
2024-05-19 01:44:43 +0100

    fix: add mongodb url

--- config/default.js
+++ config/default.js
@@ -2,7 +2,8 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 const CONFIG = {
-  PORT: process.env.PORT || 4000
+  PORT: process.env.PORT || 4000,
+  MONGODB_URL: process.env.MONGODB_URL
 }
 
 export default CONFIG
