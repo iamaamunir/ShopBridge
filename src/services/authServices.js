b5d2c1af092597767854e0dcc57f0b54b19cb4a7
Munir Abdullahi <abdullahimunir3@gmail.com>
2024-05-29 09:51:30 +0100

    feat: create user login service function

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,28 +1,48 @@
 import User from "../models/userModel.js";
-import omit from 'lodash'
 import RESPONSE_MESSAGE from "../../constant/index.js";
-import ConflictError from "../../utils/customErrors.js";
-const registerAccountService = async (payload) => {
+import * as customErrors from "../../utils/customErrors.js";
+import CONFIG from "../../config/default.js";
+import jwt from "jsonwebtoken";
+
+export const registerAccountService = async function (payload) {
   let account = await User.findOne({ email: payload.email });
   if (account) {
-    throw new ConflictError(RESPONSE_MESSAGE.CONFLICT)
+    throw new customErrors.ConflictError(RESPONSE_MESSAGE.CONFLICT);
   }
-  
+
   account = await User.create({
     firstname: payload.firstname,
     lastname: payload.lastname,
     email: payload.email,
     mobile: payload.mobile,
-    password: payload.password
-  }); 
-   const { password, ...accountWithoutPassword } = account.toObject
-     ? account.toObject()
-     : account;
-  return accountWithoutPassword;
-  // account = omit(account.toObject(), "password");
-  // return {
-  //   user: account
-  // };
-}
+    password: payload.password,
+  });
 
-  export default registerAccountService
+  // refactor code later
+  const { password,__v, ...accountWithoutPassword } = account.toObject
+    ? account.toObject()
+    : account;
+  return accountWithoutPassword;
+};
+
+export const loginAccountService = async function(payload) {
+  const user = await User.findOne({ email: payload.email });
+  if (!user) {
+    throw new customErrors.invalidCredentials(
+      RESPONSE_MESSAGE.INVALIDCRENDENTIALS
+    );
+  }
+  const validPassword = await user.comparePassword(payload.password);
+  if (validPassword) {
+    const token = jwt.sign({ id: user._id, email: user.email }, CONFIG.TOKEN_KEY, {
+      expiresIn: "2h",
+    });
+
+    user.token = token;
+    return token
+  } else {
+    throw new customErrors.invalidCredentials(RESPONSE_MESSAGE.INVALIDCRENDENTIALS);
+  }
+};
+
+// export default registerAccountService;
